ca42d548d5fe662430b60662d0a5de2f0b4cd2e2
egorrozinskiy <egor.rozinskiy@mail.ru>
2015-10-09 17:24:34 +0300

    Update A.py

--- A.py
+++ A.py
@@ -4,5 +4,7 @@ s = list(inp.readlines())
 ns = list(map(int,s[1].split(" ")))
 for i in range(int(s[0])):
   if ns.count(ns[i]) == 2:
+    wr.write(str(ns[i]))
+    wr.close()
+    inp.close()
     break
-output.close()  
