87668a0e921344df5bb8b342156d138d0b3b45db
egorrozinskiy <egor.rozinskiy@mail.ru>
2015-12-08 09:52:38 +0300

    Update D.py

--- D.py
+++ D.py
@@ -25,3 +25,15 @@
 | 6 4 6 3 |           |
 +---------+-----------+
 '''
+input=open('input.txt', 'r')
+output=open('output.txt', 'w')
+dt = list(map(int, input.readline().split()))
+k = dt[0]
+n = dt[1]
+min = [-1 for i in range(n)]
+for i in range(k):
+    Z= list(map(int, input.readline().split()))
+    for j in range(n):
+        if Z[j] < min[j] or min[j] == -1:
+            min[j] = Z[j]
+print(' '.join(map(str, min)), file=output)
